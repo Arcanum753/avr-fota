d8540d0aeda7bed8cad53ff6ec07f2704b4db9f8
Sam <Sam.Arcanum@gmail.com>
2026-02-20 19:47:02 +0300

    add post build py script

--- src/version.h
+++ src/version.h
@@ -1,18 +1,18 @@
 // Auto-generated version file
-// Generated: 2026-02-20 17:34:09
+// Generated: 2026-02-20 19:10:02
 
 #ifndef VERSION_H
 #define VERSION_H
 #define GIT_BRANCH "modules-targets-env"
-#define GIT_COMMIT "e36f131-dirty"
-#define GIT_COMMIT_FULL "e36f131a50a48ee0b800ae12fb36aa1c886b6a56-dirty"
+#define GIT_COMMIT "53ad4b9-dirty"
+#define GIT_COMMIT_FULL "53ad4b9683f8b53e866bc27f24661f9c6faa3029-dirty"
 #define GIT_TAG "no-tag"
 #define GIT_TAG_DISTANCE 0
 #define GIT_DIRTY true
-#define GIT_VERSION "modules-targets-env-e36f131-dirty-dirty"
-#define BUILD_ENV "esp8266-swd"
-#define BUILD_TIME "2026-02-20 17:34:09"
-#define BUILD_TIMESTAMP "20260220_173409"
+#define GIT_VERSION "modules-targets-env-53ad4b9-dirty-dirty"
+#define BUILD_ENV "esp32-c3-devkitm-1"
+#define BUILD_TIME "2026-02-20 19:10:02"
+#define BUILD_TIMESTAMP "20260220_191002"
 #if GIT_DIRTY
 #define IS_GIT_DIRTY true
 #else
