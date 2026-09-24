7e2feb027ed2b06a6f0ab7effcfaa21d5c8455d7
Sam <arcanum7@yandex.ru>
2026-09-25 00:12:16 +0300

    chore(versions): регенерированный core_wifi_version.h

--- src/core_wifi/core_wifi_version.h
+++ src/core_wifi/core_wifi_version.h
@@ -1,6 +1,6 @@
 // Auto-generated version file for module: core_wifi
-// Generated: 2026-09-13 13:19
-// Environment: esp32_electronica7_rgb_macros
+// Generated: 2026-09-24 23:50
+// Environment: TestCore32
 
 #ifndef CORE_WIFI_VERSION_H
 #define CORE_WIFI_VERSION_H
@@ -10,36 +10,36 @@
 // ============================================================
 
 // Числовая версия (для сравнений)
-#define CORE_WIFI_VERSION 5
+#define CORE_WIFI_VERSION 6
 
 // Строковая версия
-#define CORE_WIFI_VERSION_STR "5"
+#define CORE_WIFI_VERSION_STR "6"
 
 // Хэш последнего обработанного коммита кода (без учёта *_version.h)
-#define CORE_WIFI_COMMIT_HASH "9a24b94c834796782696b86c4c2eada2c23e54e0"
+#define CORE_WIFI_COMMIT_HASH "1f4a8929318bb1757f481ad612b8f09a5ddbdc6e"
 
 // ============================================================
 // ДАТА ПОСЛЕДНЕГО ИЗМЕНЕНИЯ
 // ============================================================
 
 // Дата в формате yyyy.mm.dd hh.mm (как строка)
-#define CORE_WIFI_COMMIT_DATE_STR "2026.09.13 00:53"
+#define CORE_WIFI_COMMIT_DATE_STR "2026.09.24 23:40"
 
 // Компоненты даты (для числовых операций)
 #define CORE_WIFI_COMMIT_YEAR 2026
 #define CORE_WIFI_COMMIT_MONTH 9
-#define CORE_WIFI_COMMIT_DAY 13
-#define CORE_WIFI_COMMIT_HOUR 0
-#define CORE_WIFI_COMMIT_MINUTE 53
+#define CORE_WIFI_COMMIT_DAY 24
+#define CORE_WIFI_COMMIT_HOUR 23
+#define CORE_WIFI_COMMIT_MINUTE 40
 
 // Полная дата в формате YYYY-MM-DD HH:MM (для отладки)
-#define CORE_WIFI_COMMIT_DATE "2026-09-13 00:53"
+#define CORE_WIFI_COMMIT_DATE "2026-09-24 23:40"
 
 // ============================================================
 // ИНФОРМАЦИЯ О ГЕНЕРАЦИИ
 // ============================================================
 
-#define CORE_WIFI_GENERATED_TIME "2026-09-13 13:19"
-#define CORE_WIFI_GENERATED_TIMESTAMP "20260913_131936"
+#define CORE_WIFI_GENERATED_TIME "2026-09-24 23:50"
+#define CORE_WIFI_GENERATED_TIMESTAMP "20260924_235039"
 
 #endif // CORE_WIFI_VERSION_H
